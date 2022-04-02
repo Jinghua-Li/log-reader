16e63436843d96f89e25fcda8cc82d3e863a9e2f
Jinghua Li <jinhli@thoughtworks.com>
2022-04-02 14:51:35 +0800

    Refactor search log

--- src/main/java/com/log/service/LogReaderService.java
+++ src/main/java/com/log/service/LogReaderService.java
@@ -3,7 +3,6 @@ package com.log.service;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.log.model.Log;
 import com.log.model.LogSearch;
@@ -19,15 +18,11 @@ public class LogReaderService {
     private LogRepository logRepository;
     
     public List<LogResponse> searchLog(LogSearch search) {
-        final List<Log> allLogs = logRepository.findAllLogs();
-        return filterLogs(search, allLogs).sorted(Comparator.comparing(Log::getDateTime).reversed())
+        return logRepository.findAllLogs().stream()
+                .filter(log -> log.getDateTime().isBefore(search.getEndTime()))
+                .filter(log -> search.getStartTime().map(startTime -> log.getDateTime().isAfter(startTime)).orElse(true))
+                .filter(log -> search.getType().map(type -> type == log.getType()).orElse(true))
+                .sorted(Comparator.comparing(Log::getDateTime).reversed())
                 .map(Log::toLogResponse).collect(Collectors.toList());
-    }
-    
-    private static Stream<Log> filterLogs(LogSearch search, List<Log> allLogs) {
-        return allLogs.stream().filter(log -> log.getDateTime().isBefore(search.getEndTime()))
-                .filter(log -> search.getStartTime().map(startTime -> log.getDateTime().isAfter(startTime))
-                        .orElse(true))
-                .filter(log -> search.getType().map(type -> type == log.getType()).orElse(true));
     }
 }
