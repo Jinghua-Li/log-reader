7c02bb8752e07ff59f1142d83a5ddcc8f56083c7
Jinghua Li <jinhli@thoughtworks.com>
2022-04-02 23:19:29 +0800

    Add log parse utils

--- src/main/java/com/log/utils/Constants.java
+++ src/main/java/com/log/utils/Constants.java
@@ -1,7 +1,12 @@
 package com.log.utils;
 
+import java.util.regex.Pattern;
+
 public class Constants {
     public static final String DATE_FORMATTER = "yyyy-MM-dd HH:mm:ss";
     public static final String ERROR_DATA_FORMATTER = "日期格式错误，请输入正确的日期时间，例如：2022-04-01 11:53:03";
     public static final String ERROR_LOG_TYPE = "日志类型输入错误，请输入正确的日志类型：ERROR, INFO, WARNING, DEBUG, ALL";
+    public static final Pattern DATE_PATTERN = Pattern.compile("\\d{4}-\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}");
+    public static final Pattern TYPE_PATTERN = Pattern.compile("ERROR|INFO|WARNING|DEBUG");
+    public static final Pattern THREAD_PATTERN = Pattern.compile("\\[(.*?)]");
 }
---
+++ src/main/java/com/log/utils/LogFileParseUtils.java
@@ -0,0 +1,41 @@
+package com.log.utils;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.regex.Matcher;
+
+import com.log.model.LogType;
+
+public class LogFileParseUtils {
+    public static Optional<LocalDateTime> extractDateTime(String logStr) {
+        final Matcher matcher = Constants.DATE_PATTERN.matcher(logStr);
+        if (matcher.find()) {
+            return DateFormatter.convertToDate(Optional.of(matcher.group(0)));
+        }
+        return Optional.empty();
+    }
+    
+    public static Optional<LogType> extractLogType(String logStr) {
+        final Matcher matcher = Constants.TYPE_PATTERN.matcher(logStr);
+        if (matcher.find()) {
+            return Optional.of(LogType.valueOf(matcher.group(0)));
+        }
+        return Optional.empty();
+    }
+    
+    public static Optional<String> extractThreadId(String logStr) {
+        final Matcher matcher = Constants.THREAD_PATTERN.matcher(logStr);
+        if (matcher.find()) {
+            return Optional.of(matcher.group(0).replace("[", "").replace("]", "").trim());
+        }
+        return Optional.empty();
+    }
+    
+    public static String extractMessage(String logStr) {
+        final String[] messages = logStr.split(":");
+        if (messages.length > 0) {
+            return messages[messages.length - 1].trim();
+        }
+        return logStr.trim();
+    }
+}
