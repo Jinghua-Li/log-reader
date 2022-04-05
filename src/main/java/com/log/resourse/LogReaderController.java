2edef7f584d1aeeb0a670b4703eb82e69bca9aec
Jinghua Li <jinhli@thoughtworks.com>
2022-04-05 12:39:48 +0800

    remove all implements for tests

--- src/main/java/com/log/resourse/LogReaderController.java
+++ src/main/java/com/log/resourse/LogReaderController.java
@@ -1,16 +1,9 @@
 package com.log.resourse;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Optional;
 
-import com.log.exception.BadRequestException;
-import com.log.model.LogSearch;
-import com.log.model.LogType;
 import com.log.service.LogReaderService;
-import com.log.utils.Constants;
-import com.log.utils.DateFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,17 +19,6 @@ public class LogReaderController {
     public List<LogResponse> searchLog(@RequestParam(name = "type", required = false) Optional<String> type,
                                        @RequestParam(value = "startTime", required = false) Optional<String> startTime,
                                        @RequestParam(value = "endTime", required = false) Optional<String> endTime) {
-        try {
-            LogSearch logSearch = LogSearch.builder()
-                    .type(type.map(LogType::valueOf))
-                    .startTime(DateFormatter.convertToDate(startTime))
-                    .endTime(DateFormatter.convertToDate(endTime).orElse(LocalDateTime.now()))
-                    .build();
-            return logReaderService.searchLog(logSearch);
-        } catch (DateTimeParseException exception) {
-            throw new BadRequestException(Constants.ERROR_DATA_FORMATTER);
-        } catch (IllegalArgumentException exception) {
-            throw new BadRequestException(Constants.ERROR_LOG_TYPE);
-        }
+        return null;
     }
 }
--- src/main/java/com/log/service/LogReaderService.java
+++ src/main/java/com/log/service/LogReaderService.java
@@ -1,10 +1,7 @@
 package com.log.service;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import com.log.model.Log;
 import com.log.model.LogSearch;
 import com.log.resourse.LogResponse;
 import com.log.respository.LogRepository;
@@ -18,11 +15,6 @@ public class LogReaderService {
     private LogRepository logRepository;
     
     public List<LogResponse> searchLog(LogSearch search) {
-        return logRepository.findAllLogs().stream()
-                .filter(log -> log.getDateTime().isBefore(search.getEndTime()))
-                .filter(log -> search.getStartTime().map(startTime -> log.getDateTime().isAfter(startTime)).orElse(true))
-                .filter(log -> search.getType().map(type -> type == log.getType()).orElse(true))
-                .sorted(Comparator.comparing(Log::getDateTime).reversed())
-                .map(Log::toLogResponse).collect(Collectors.toList());
+        return null;
     }
 }
